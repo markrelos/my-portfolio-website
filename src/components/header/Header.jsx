7f47d706827f91db4d7a02f93922cdd2ab06c0c7
Mark <relos1203@gmail.com>
2023-03-06 23:28:39 +0800

    deleted unnecessary comments

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,40 +12,19 @@ const Header = () => {
   return (
     <header className="fade-in">
       <div className="container header__container">
-      <div className="letter-container">
-          {sentence.map((word, wordIndex) => (
-            <div key={wordIndex}>
-              {word.split("").map((letter, letterIndex) => (
-                <TextSpan key={letterIndex}>
-                  {letter === ' ' ? '\u00A0' : letter}
-                </TextSpan>
-              ))}
-            </div>
-          ))}
+        <div className="letter-container">    
+                {sentence.map((word, wordIndex) => (
+                  <div key={wordIndex}>
+                    {word.split("").map((letter, letterIndex) => (
+                      <TextSpan key={letterIndex}>
+                        {letter === ' ' ? '\u00A0' : letter}
+                      </TextSpan>
+                    ))}
+                  </div>
+                ))}
         </div>
-      <p className="text_light"> Frontend / Mobile Game Developer </p>
-        {/*<h5>Hello I'm</h5>
-        <h1>Mark Relos</h1>
-        <h2 className="text-light">Fullstack Developer</h2>*/}
-        {/*<CTA />*/}
-        
+        <p className="text_light" alt="me"> Frontend / Mobile Game Developer </p>        
         <HeaderSocials />
-        <motion.div 
-        intial={{
-          x: -500,
-          opacity: 0,
-          scale: 0.5
-        }}
-        
-        animate={{
-          x: 0,
-          opacity: 1,
-          scale: 1,
-        }}
-        transition={{duration: 3}}
-        className="me">
-          {/*<img src={ME} alt="me" />*/}
-        </motion.div >
         <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
