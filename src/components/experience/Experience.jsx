783144616ee06ae7cd9f85b0127a517bca00241a
Mark <relos1203@gmail.com>
2023-03-07 19:22:48 +0800

    Added Animation pt.5

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -42,106 +42,106 @@ const Experience = () => {
         })}
         </div>
       <div className="container experience__container">
-        <motion.div className="animate"
-            initial={{ scale: 0 }}
-            animate={isVisible ? {scale: 1 } : { scale: 0 }}
-            transition={{ type: 'spring', stiffness: 260, damping: 20}}
-        >
-          <div>
-          <h3>Frontend Development</h3>
-          <div className="experience__content">
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
-              <div>
-              <h4>HTML</h4>
-              <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-             <div>
-             <h4>CSS</h4>
-              <small className='text-light'>Intermediate</small>
-             </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-              <h4>JavaScript</h4>
-              <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-              <h4>Bootstrap</h4>
-              <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-              <h4>C#</h4>
-              <small className='text-light'>Expert</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-              <h4>React</h4>
-              <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-          </div>
-          </div>
-        </motion.div>
+      <div>
+          <motion.div className="animate"
+              initial={{ scale: 0 }}
+              animate={isVisible ? {scale: 1 } : { scale: 0 }}
+              transition={{ type: 'spring', stiffness: 260, damping: 20}}
+          >
+                <h3>Frontend Development</h3>
+                <div className="experience__content">
+                      <article className='experience__details'>
+                        <BsPatchCheckFill className='experience__details-icon' />
+                        <div>
+                        <h4>HTML</h4>
+                        <small className='text-light'>Experienced</small>
+                        </div>
+                      </article>
+                      <article className='experience__details'>
+                        <BsPatchCheckFill className='experience__details-icon'/>
+                      <div>
+                      <h4>CSS</h4>
+                        <small className='text-light'>Intermediate</small>
+                      </div>
+                      </article>
+                      <article className='experience__details'>
+                        <BsPatchCheckFill className='experience__details-icon'/>
+                        <div>
+                        <h4>JavaScript</h4>
+                        <small className='text-light'>Experienced</small>
+                        </div>
+                      </article>
+                      <article className='experience__details'>
+                        <BsPatchCheckFill className='experience__details-icon'/>
+                        <div>
+                        <h4>Bootstrap</h4>
+                        <small className='text-light'>Experienced</small>
+                        </div>
+                      </article>
+                      <article className='experience__details'>
+                        <BsPatchCheckFill className='experience__details-icon'/>
+                        <div>
+                        <h4>C#</h4>
+                        <small className='text-light'>Expert</small>
+                        </div>
+                      </article>
+                      <article className='experience__details'>
+                        <BsPatchCheckFill className='experience__details-icon'/>
+                        <div>
+                        <h4>React</h4>
+                        <small className='text-light'>Experienced</small>
+                        </div>
+                      </article>
+                </div>
+          </motion.div>
+        </div>
           {/* END OF FRONTEND */}
-        <motion.div className="animate"
-            initial={{scale: 0 }}
-            animate={isVisible ? {scale: 1 } : { scale: 0 }}
-            transition={{ type: 'spring', stiffness: 260, damping: 20, delay: 0.2 }}
-        >
-          <div>
-        <h3>Backend Development</h3>
-          <div className="experience__content">
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-              <h4>Node JS</h4>
-              <small className='text-light'>Experienced</small>
+        <div>
+            <motion.div className="animate"
+                initial={{scale: 0 }}
+                animate={isVisible ? {scale: 1 } : { scale: 0 }}
+                transition={{ type: 'spring', stiffness: 260, damping: 20, delay: 0.2 }}
+            >
+            <h3>Backend Development</h3>
+              <div className="experience__content">
+                <article className='experience__details'>
+                  <BsPatchCheckFill className='experience__details-icon'/>
+                  <div>
+                  <h4>Node JS</h4>
+                  <small className='text-light'>Experienced</small>
+                  </div>
+                </article>
+                <article className='experience__details'>
+                  <BsPatchCheckFill className='experience__details-icon'/>
+                  <div>
+                  <h4>MongoDB</h4>
+                  <small className='text-light'>Intermediate</small>
+                  </div>
+                </article>
+                <article className='experience__details'>
+                  <BsPatchCheckFill className='experience__details-icon'/>
+                  <div>
+                  <h4>PHP</h4>
+                  <small className='text-light'>Intermediate</small>
+                  </div>
+                </article>
+                <article className='experience__details'>
+                  <BsPatchCheckFill className='experience__details-icon'/>
+                  <div>
+                  <h4>MySQL</h4>
+                  <small className='text-light'>Intermediate</small>
+                  </div>
+                </article>
+                <article className='experience__details'>
+                  <BsPatchCheckFill className='experience__details-icon'/>
+                <div>
+                  <h4>Python</h4>
+                  <small className='text-light'>Experienced</small>
+                </div>
+                </article>
               </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-              <h4>MongoDB</h4>
-              <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-              <h4>PHP</h4>
-              <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-              <h4>MySQL</h4>
-              <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-             <div>
-               <h4>Python</h4>
-              <small className='text-light'>Experienced</small>
-             </div>
-            </article>
-          </div>
-          </div>
-        </motion.div>
+            </motion.div>
+        </div>
       </div>
     </section>
   )
